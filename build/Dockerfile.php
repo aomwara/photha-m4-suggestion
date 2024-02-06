b620d034548cc4cc8d8f430d7bd96e5707b9cf25
aomwara <waranat.cpe@gmail.com>
2024-02-06 21:52:33 +0700

    fix: docker internal user

--- build/Dockerfile.php
+++ build/Dockerfile.php
@@ -3,11 +3,11 @@ FROM php:5-fpm-alpine
 ADD ./php/www.conf /usr/local/etc/php-fpm.d/www.conf
 ADD ./php/php.ini /usr/local/etc/php/php.ini
 
-RUN addgroup -g 1000 smos && adduser -G smos -g smos -s /bin/sh -D smos
+RUN addgroup -g 1000 photha && adduser -G photha -g photha -s /bin/sh -D photha
 
 RUN mkdir -p /var/www/html
 
-RUN chown smos:smos /var/www/html
+RUN chown photha:photha /var/www/html
 
 WORKDIR /var/www/html
 
